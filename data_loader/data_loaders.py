24661d70fdf5a103e8e143b6c10adeeab2fa37c4
Santhosh <santhosn@usc.edu>
2019-05-25 21:15:12 +0000

    add few transformations

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -9,9 +9,10 @@ class DataLoader(BaseDataLoader):
         
         # transformations to be applied
         trsfm = transforms.Compose([
-                    transforms.Resize(input_size),
+#                     transforms.Resize(input_size),
+                    transforms.RandomHorizontalFlip(),
                     transforms.ToTensor(),
-                    transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5])
+                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
                 ])
         
         # path of the text file that consists each image name and label
@@ -23,4 +24,22 @@ class DataLoader(BaseDataLoader):
         # passing dataset object along with other parameters 
         super(DataLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
         
+class TestLoader(BaseDataLoader):
     
+    def __init__(self, data_dir, file, batch_size, shuffle, validation_split, num_workers, input_size):
+        
+        # transformations to be applied
+        trsfm = transforms.Compose([
+#                     transforms.Resize(input_size),
+                    transforms.ToTensor(),
+                    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+                ])
+        
+        # path of the text file that consists each image name and label
+        self.data_dir = data_dir
+        
+        # creating the dataset object
+        self.dataset = ImageDataset(self.data_dir, file, transform = trsfm)
+        
+        # passing dataset object along with other parameters 
+        super(TestLoader, self).__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
